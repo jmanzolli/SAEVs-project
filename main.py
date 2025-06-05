5895e9c36c1edcebd43e128f79098af7cd5994d5
Jônatas Augusto Manzolli <johjam@gmail.com>
2025-06-05 17:05:40 -0400

    updated main

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ model = solve_opt(data,pass_min=2000, mult_2=0,tee=True)
 # Plot the results
 D_passenger = (data['Total demand'][:96]).tolist()
 O_energy = (data['Outage'][:96]).tolist()
-#plot_variables(model, D_passenger, O_energy)
+plot_variables(model, D_passenger, O_energy)
 
 # Save results to Excel
 filename = 'test.xlsx'
-#save_to_excel(model, filename=filename)
+save_to_excel(model, filename=filename)
